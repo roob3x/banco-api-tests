2fb76cc93a10932d480166cbb9461f18a7e07683
Roberto Filho <roberto.siilva@hotmail.com>
2026-04-17 15:53:06 -0300

    added tests /get transferencias total and individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -6,6 +6,13 @@ import { obterToken } from '../helpers/autenticacao.js';
 import postTransferencias from '../fixtures/postTransferencias.json' assert { type: 'json' };
 
 describe('Transferencias', () => {
+    let token
+
+    beforeEach(async () => {
+
+        token = await obterToken('julio.lima', '123456')
+
+    })
     describe('POST /transferencias', async() => {
         let token
 
@@ -46,6 +53,35 @@ describe('Transferencias', () => {
             })
             expect(response.status).to.equal(422);
             console.log(response.body)
+        })
+    })
+
+    describe('GET /transferencias/{id}', () => {
+        it('Deve retornar sucesso 200 e dados iguais ao degistro de transferencias contigos no bano de dado quando o id for valido', async () => {
+            const response = await request(process.env.BASE_URL)
+                .get('/transferencias/1')
+                .set('Authorization', `Bearer ${token}`)
+            console.log(response.status)
+            console.log(response.body)
+            expect(response.status).to.equal(200)
+            expect(response.body.id).to.equal(1)
+            expect(response.body.id).to.be.a('number')
+            expect(response.body.conta_origem_id).to.equal(1)
+            expect(response.body.conta_destino_id).to.equal(2)
+            expect(response.body.valor).to.equal(10.00)
+
+        })
+        
+    })
+
+    describe('GET /transferencias', () => {
+        it('Deve retornar 10 elementos na paginacao quando informar limite de 10 registros', async () => {
+            const response = await request(process.env.BASE_URL)
+                .get('/transferencias?page=1&limit=10')
+                .set('Authorization', `Bearer ${token}`)
+            expect(response.status).to.equal(200)
+            expect(response.body.limit).to.equal(10)
+            expect(response.body.transferencias).to.have.lengthOf(10)
         })
     })
 
